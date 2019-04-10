857c37fb2604ff921493d821c376a31dac74f2bd
cj <cjlazell@gmail.com>
2019-04-09 21:54:28 -0500

    polyfill dynamic import as Firefox and Edge do not support it yet.

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,10 +1,11 @@
 import html from '/html.js'
 import { useCallback } from '/web_modules/preact/hooks.js'
 import PreactRouter from '/web_modules/preact-router.js'
-import Async from '/lib/async.js'
+import Async from '/components/Async.js'
 import eventBus from '/lib/event-bus.js'
+import asyncImport from '/lib/async-import.js'
 
-const Home = () => import(`./Home.js`)
+const Home = () => asyncImport(`/routes/Home.js`)
 
 export const Router = () => {
   return html`
